de67bf5a1223e382081f757cc1811cdfef35eced
John Ryan <johnjoseph.ryan0@gmail.com>
2025-04-17 14:05:36 -0700

    Update dim_borough.sql

--- models/marts/dim/dim_borough.sql
+++ models/marts/dim/dim_borough.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT pickup_borough AS borough FROM {{ ref('stg_nyc_taxi_trips') }}
 UNION
-SELECT DISTINCT dropoff_borough FROM {{ ref('stg_nyc_taxi_trips') }};
+SELECT DISTINCT dropoff_borough FROM {{ ref('stg_nyc_taxi_trips') }}
