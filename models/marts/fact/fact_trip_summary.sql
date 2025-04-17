76e2aa5e2c0ac7d60a8ba23e9893a6adbb770a32
John Ryan <johnjoseph.ryan0@gmail.com>
2025-04-17 14:07:03 -0700

    Update fact_trip_summary.sql

--- models/marts/fact/fact_trip_summary.sql
+++ models/marts/fact/fact_trip_summary.sql
@@ -3,6 +3,6 @@ SELECT
     dropoff_borough,
     COUNT(*) AS total_trips,
     ROUND(AVG(fare_amount), 2) AS avg_fare,
-    ROUND(AVG(trip_duration_min), 2) AS avg_trip_duration
+    ROUND(AVG(trip_duration_min)::numeric, 2) AS avg_trip_duration
 FROM {{ ref('stg_nyc_taxi_trips') }}
 GROUP BY pickup_borough, dropoff_borough
